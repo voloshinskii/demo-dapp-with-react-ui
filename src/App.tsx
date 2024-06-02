ba67075380ed2b5498131d54d5b101c4345803fa
Max Voloshinskii <me@voloshinskii.ru>
2024-06-02 19:28:53 +0300

    Bring back tonproof

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ function App() {
         <div className="app">
             <Header />
             <TxForm />
+            <TonProofDemo />
             <Footer />
         </div>
       </TonConnectUIProvider>
